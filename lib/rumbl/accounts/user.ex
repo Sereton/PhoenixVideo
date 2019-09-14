1affbf979d0666753b4c9c5db7dfaf6e8e34a582
Jay Hernandez <ajuan_jose@hotmail.com>
2019-09-14 05:05:36 -0600

    Users resources added

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -1,5 +1,23 @@
 defmodule Rumbl.Accounts.User do
 
+use Ecto.Schema
+import Ecto.Changeset
 
-defstruct [:id, :name, :username]
+schema "users" do
+field :name, :string
+field :username, :string
+
+timestamps()
+
+end
+
+def changeset(user, attrs) do
+  user
+  |> cast(attrs, [:name, :username])
+  |> validate_required([:name, :username])
+  |> validate_length(:username, min: 1, max: 20)
+end
+
+
+
 end
